80e84cffd949289f635ad8e6f238e5d32aa55508
Frank Edwards <frankedwards50@gmail.com>
2022-03-04 23:15:20 -0800

    Tray functionality and taskbar icon

--- main.js
+++ main.js
@@ -1,43 +1,87 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Tray, nativeImage } = require("electron");
+const path = require("path");
 
-function createWindow () {
+let tray, window;
+
+//app.dock.hide()
+
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  window = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: true,
+    fullscreenable: true,
+    resizable: true,
+    transparent: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+    icon: path.join(__dirname, "public/images/icon.ico"),
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000')
+  window.on("closed", () => (window = null));
+
+  window.loadURL("http://localhost:3000");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
+const createTray = () => {
+  const iconPath = path.join(__dirname, "public/images/icon.ico");
+  const icon = nativeImage.createFromPath(iconPath);
+
+  tray = new Tray(icon);
+  tray.on("click", (event) => toggleWindow());
+};
+
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+};
+
+const showWindow = () => {
+  const position = windowPosition();
+  if (process.platform === "darwin") {
+    window.setPosition(position.x, position.y);
+  }
+
+  window.show();
+};
+
+const windowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+
+  const x = Math.round(
+    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+  );
+  const y = Math.round(trayBounds.y + trayBounds.height);
+
+  return { x, y };
+};
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createTray();
+  createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
