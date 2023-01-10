c262470e7ae69afe146536a63838b0fcf22c4143
Frank Edwards <frankedwards50@gmail.com>
2023-01-10 02:28:12 -0800

    Still testing autoupdate bug fix

--- public/electron/functions/launch/handleLaunch.js
+++ public/electron/functions/launch/handleLaunch.js
@@ -1,5 +1,5 @@
 // Library Imports
-const { app, BrowserWindow, dialog } = require("electron");
+const { app, BrowserWindow } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
 //Functions
@@ -39,31 +39,6 @@ const handleLaunch = (tray, window, store, directoryName) => {
 
   if (isDev === false) {
     autoUpdater.checkForUpdates();
-
-    /* autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-      const dialogOpts = {
-        type: "info",
-        buttons: ["Ok"],
-        title: "Application Update",
-        message: process.platform === "win32" ? releaseNotes : releaseName,
-        detail: "A new version is being downloaded.",
-      };
-      dialog.showMessageBox(dialogOpts, (response) => {});
-    });
-
-    autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
-      const dialogOpts = {
-        type: "info",
-        buttons: ["Restart", "Later"],
-        title: "Application Update",
-        message: process.platform === "win32" ? releaseNotes : releaseName,
-        detail:
-          "A new version has been downloaded. Restart the application to apply the updates.",
-      };
-      dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall();
-      });
-    }); */
   }
 };
 
